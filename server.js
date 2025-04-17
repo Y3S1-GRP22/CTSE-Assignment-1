3968c8dba80a2e9cc4c1b52f962dcf8a4d60e51d
Sujitha1221 <sujikanthan2001@gmail.com>
2025-04-17 11:05:12 +0530

    feat: add helmet middleware for enhanced security

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const databaseConnection = require("./config/dbConnection");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 80;
 
+app.use(helmet());
 app.use(express.json());
+
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:4000"],
@@ -14,9 +19,12 @@ app.use(
   })
 );
 
-app.use("/", (req, res) => {
-   res.json({ msg: "Hey, CTSE Deployment is working!" });
- });
+// Health check 
+app.get("/", (req, res) => {
+  res.json({ msg: "Hey, CTSE Deployment is working!" });
+});
+
+// User route
 app.use("/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
